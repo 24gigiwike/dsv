c64574a321e99a80d0f27fd86cbc138d5f90ae40
24gigiwike <70345139+24gigiwike@users.noreply.github.com>
2025-09-21 15:31:56 +0100

    Update header.tsx

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -30,6 +30,19 @@ export function Header() {
           </Link>
         </div>
       </div>
+      <!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'b5128a73cc5937dbd8350f946eb65317e4a26df6';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
+        
     </header>
   );
 }
